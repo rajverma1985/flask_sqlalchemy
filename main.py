02e726b81d2f56fa10c3f28ac792cdb1a101ab13
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-12-25 23:55:01 +0530

    db stuff one to many

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.sqlite3'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Employer(db.Model):
+    __tablename__ = 'company'
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(20))
+    employees = db.Column(db.Integer)
+    emp_info = db.relationship('Employee', backref='company')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    age = db.Column(db.Integer)
+    designation = db.Column(db.String(15))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+with app.app_context():
+    db.create_all()
+    Migrate(app, db, render_as_batch=True)
+
+
+@app.route('/')
+def add_company():
+    target = Employer(company_name='target', employees=2000)
+    anz = Employer(company_name='anz', employees=50000)
+    dell = Employer(company_name='dell', employees=96000)
+    microsoft = Employer(company_name='microsoft', employees=1200)
+    db.session.add_all([target, anz, dell, microsoft])
+    db.session.commit()
+    return "Data added"
+
+
+@app.route('/e')
+def add_employee():
+    ram = Employee(name='ram', age=35, designation='engineer', company='target')
+    rupal = Employee(name='rupal', age=23, designation='Lead engineer', company='anz')
+    chaman = Employee(name='chaman', age=35, designation='Manager', company='dell')
+    abhishek = Employee(name='abhishek', age=35, designation='Director', company='microsoft')
+    db.session.add_all([ram, rupal, chaman, abhishek])
+    db.session.commit()
+    return "Data added"
+
+
+if __name__ == "__main__":
+    app.run()
